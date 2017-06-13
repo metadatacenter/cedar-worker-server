ec6c329bc3e95bf7bc1aa0922a3c1fedcfa3fe69
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-06-13 11:08:52 -0700

    ENV vars added to tests

--- cedar-worker-server-application/src/test/java/org/metadatacenter/cedar/worker/config/CedarConfigWorkerTest.java
+++ cedar-worker-server-application/src/test/java/org/metadatacenter/cedar/worker/config/CedarConfigWorkerTest.java
@@ -20,6 +20,8 @@ public class CedarConfigWorkerTest {
 
     env.put(CedarEnvironmentVariable.CEDAR_HOST.getName(), "metadatacenter.orgx");
 
+    env.put(CedarEnvironmentVariable.CEDAR_NET_GATEWAY.getName(), "127.0.0.1");
+
     env.put(CedarEnvironmentVariable.CEDAR_ADMIN_USER_API_KEY.getName(), "1234");
 
     env.put(CedarEnvironmentVariable.CEDAR_NEO4J_USER_PASSWORD.getName(), "password");
@@ -40,6 +42,10 @@ public class CedarConfigWorkerTest {
     env.put(CedarEnvironmentVariable.CEDAR_WORKER_HTTP_PORT.getName(), "9011");
     env.put(CedarEnvironmentVariable.CEDAR_WORKER_ADMIN_PORT.getName(), "9111");
     env.put(CedarEnvironmentVariable.CEDAR_WORKER_STOP_PORT.getName(), "9211");
+
+    env.put(CedarEnvironmentVariable.CEDAR_FOLDER_HTTP_PORT.getName(), "9008");
+    env.put(CedarEnvironmentVariable.CEDAR_TEMPLATE_HTTP_PORT.getName(), "9001");
+    env.put(CedarEnvironmentVariable.CEDAR_USER_HTTP_PORT.getName(), "9005");
 
     TestUtil.setEnv(env);
   }
