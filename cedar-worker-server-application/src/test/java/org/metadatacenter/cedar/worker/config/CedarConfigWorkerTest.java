de90431c9e91cb6e9c0684bb9fcfaab189c02ca2
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-06-15 15:15:29 -0700

    Fixed elastic transport port

--- cedar-worker-server-application/src/test/java/org/metadatacenter/cedar/worker/config/CedarConfigWorkerTest.java
+++ cedar-worker-server-application/src/test/java/org/metadatacenter/cedar/worker/config/CedarConfigWorkerTest.java
@@ -34,7 +34,7 @@ public class CedarConfigWorkerTest {
     env.put(CedarEnvironmentVariable.CEDAR_MONGO_PORT.getName(), "27017");
 
     env.put(CedarEnvironmentVariable.CEDAR_ELASTICSEARCH_HOST.getName(), "127.0.0.1");
-    env.put(CedarEnvironmentVariable.CEDAR_ELASTICSEARCH_TRANSPORT_PORT.getName(), "9200");
+    env.put(CedarEnvironmentVariable.CEDAR_ELASTICSEARCH_TRANSPORT_PORT.getName(), "9300");
 
     env.put(CedarEnvironmentVariable.CEDAR_REDIS_PERSISTENT_HOST.getName(), "127.0.0.1");
     env.put(CedarEnvironmentVariable.CEDAR_REDIS_PERSISTENT_PORT.getName(), "6379");
